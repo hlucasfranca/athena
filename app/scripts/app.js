100180f4bc506481298160da04f719ba9e4f3720
henrique <hlucasfranca@gmail.com>
2015-06-27 15:43:42 -0300

    Corrected indentation. Added contact and graph views routes.  Main page content changed to reflect app purpose.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,25 +9,34 @@
  * Main module of the application.
  */
 angular
-  .module('grapheApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('grapheApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/graph', {
+                templateUrl: 'views/graph.html',
+                controller: 'GraphCtrl'
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'ContactCtrl'
+            })
+
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,11 @@
  * Controller of the grapheApp
  */
 angular.module('grapheApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+    });
