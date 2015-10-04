11350b96b536ff212d3cb692df4c5ef225be8c1e
henrique <hlucasfranca>
2015-10-04 10:42:09 -0300

    Removed unused code.

--- app/scripts/algorithms/dfs.js
+++ app/scripts/algorithms/dfs.js
@@ -5,6 +5,14 @@
         .service('dfs', function () {
             var service = {};
 
+            function next(){
+
+            }
+
+            function previous(){
+
+            }
+
             var n = 'Depth first search',
                 instructions = [],
                 result = [],
@@ -15,34 +23,10 @@
                     'if vertex w is not labeled as discovered then',
                     'recursively call DFS(G,w)',
                     'end of algorithm'
-                ];
+                ],
+                stepList = [];
 
-            /*
-             *  Method definitions
-             * */
 
-            function depthSearch(graph, visited, dfsSteps) {
-                dfsSteps.push({instruction: 0, visited: visited});
-                visited.marked = true;
-                dfsSteps.push({instruction: 1, visited: visited});
-
-                graph.adjacentList[visited.index].forEach(function (node) {
-                    dfsSteps.push({instruction: 2, visited: visited});
-                    dfsSteps.push({instruction: 3, visited: visited});
-
-                    if (!node.marked) {
-                        dfsSteps.push({instruction: 4, visited: visited});
-                        service.run(graph, node, dfsSteps);
-                    }
-                });
-
-                dfsSteps.push({instruction: 5, visited: visited});
-
-                // clean markers
-                graph.getNodes().forEach(function (element) {
-                    element.marked = false;
-                });
-            }
 
             function run(graph, visited) {
 
@@ -71,7 +55,6 @@
                 name: n,
                 steps: steps,
                 instructions: instructions,
-                depthSearch: depthSearch,
                 run: run,
                 result: result
             };
